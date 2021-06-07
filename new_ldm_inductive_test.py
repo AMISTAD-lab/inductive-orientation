d1b3ab3d0ed72caefd8738eee345763e553d5dd2
echen4628 <76180196+echen4628@users.noreply.github.com>
2021-06-07 13:46:06 -0700

    changed classes

--- new_ldm_inductive_test.py
+++ new_ldm_inductive_test.py
@@ -19,16 +19,17 @@ adaboostClassifier = AdaBoostClassifier()
 holdout_set_percentage = 0.003 #i think this is useless now rt
 num_datasets = 5
 proportion_of_dataset = 0.3
+classes = [0,1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.003) #gives us about 5 things in the holdout set
 #X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size = holdout_set_percentage)
 
-LDM = ldm_inductive.getLDM(model, X_train, X_test, y_train)
+LDM = ldm_inductive.getLDM(model, X_train, X_test, y_train, classes=classes)
 print(LDM)
 Pd = ldm_inductive.computePD(LDM)
 
-list_of_PD_sparse = ldm_inductive.computeNPD(5, model, X_train, X_test, y_train)
-list_of_PD = ldm_inductive.computeNPD(5, model, X_train, X_test, y_train, sparse=False)
+list_of_PD_sparse = ldm_inductive.computeNPD(5, model, X_train, X_test, y_train, classes= classes)
+list_of_PD = ldm_inductive.computeNPD(5, model, X_train, X_test, y_train, sparse=False, classes=classes)
 
 print("sparse")
 ldm_inductive.varianceUpToN(list_of_PD_sparse)
