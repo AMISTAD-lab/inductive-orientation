1900587f6e91a0f86f671560d89121a2b6d3b607
echen4628 <76180196+echen4628@users.noreply.github.com>
2021-06-08 13:41:32 -0700

    added variance and rmse

--- new_ldm_inductive_test.py
+++ new_ldm_inductive_test.py
@@ -3,6 +3,7 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import new_ldm_inductive as ldm_inductive
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,10 +13,11 @@ import pandas as pd
 dataset = pd.read_csv("BankNote_Authentication.csv")
 X = dataset[dataset.columns[0:4]]
 y = dataset[dataset.columns[4]]
-model = KNeighborsClassifier(n_neighbors=1)
-model3 = KNeighborsClassifier(n_neighbors=3)
-model10 = KNeighborsClassifier(n_neighbors=10)
-adaboostClassifier = AdaBoostClassifier()
+gaussianNaiveBayes = GaussianNB()
+# model = KNeighborsClassifier(n_neighbors=1)
+# model3 = KNeighborsClassifier(n_neighbors=3)
+# model10 = KNeighborsClassifier(n_neighbors=10)
+# adaboostClassifier = AdaBoostClassifier()
 # holdout_set_percentage = 0.003 #i think this is useless now rt
 # num_datasets = 5
 # proportion_of_dataset = 0.3
@@ -35,23 +37,41 @@ def trial(clf):
     sparse_Pd_l = [ldm_inductive.computePD(LDM) for LDM in list_of_LDM]
     predict_proba_Pd_l = ldm_inductive.computeNPD(N, clf, X_train, X_test, y_train, classes=classes, num_datasets=num_datasets, proportion_of_dataset=proportion_of_dataset, sparse=False)
     SGT_Pd_l = [ldm_inductive.simpleGoodTuring(LDM) for LDM in list_of_LDM]
-    #sparse_LDM = ldm_inductive.getLDM(clf, X_train, X_test, y_train, classes=classes, num_datasets=num_datasets, proportion_of_dataset=proportion_of_dataset)
-    #sparse_Pd = ldm_inductive.computePD(sparse_LDM)
+
+    print("example inductive orientation vector -------------------------------------------------------------")
+
     sparse_Pd = sparse_Pd_l[0]
-    print("sparse PD:", sparse_Pd)
-    #predict_proba_LDM = ldm_inductive.getLDM(clf, X_train, X_test, y_train, classes=classes, num_datasets=num_datasets, proportion_of_dataset=proportion_of_dataset,sparse=False)
-    #predict_proba_Pd = ldm_inductive.computePD(predict_proba_LDM)
+    print("example sparse PD:", sparse_Pd)
     predict_proba_Pd = predict_proba_Pd_l[0]
-    print("predict_proba PD:", predict_proba_Pd)
-    #SGT_Pd = ldm_inductive.simpleGoodTuring(sparse_LDM)
+    print("example predict_proba PD:", predict_proba_Pd)
     SGT_Pd = SGT_Pd_l[0]
-    print("SGT PD:", SGT_Pd)
+    print("example SGT PD:", SGT_Pd)
+
+    print("variance -------------------------------------------------------------")
+
+    sparse_var = ldm_inductive.computeVariance(sparse_Pd_l)
+    print("sparse PD variance: ", sparse_var)
+    predict_proba_var = ldm_inductive.computeVariance(predict_proba_Pd_l)
+    print("predict_proba PD variance: ", predict_proba_var)
+    SGT_var = ldm_inductive.computeVariance(SGT_Pd_l)
+    print("SGT adjusted PD variance: ", SGT_var)
+
+    print("angle -------------------------------------------------------------")
+
     print("angle sparse, SGT: ", ldm_inductive.computeAngle(sparse_Pd, SGT_Pd))
     print("angle sparse, predict_proba: ", ldm_inductive.computeAngle(sparse_Pd, predict_proba_Pd))
     print("angle predict_proba, SGT: ", ldm_inductive.computeAngle(predict_proba_Pd, SGT_Pd))
-    #-----------------------------------------------
+    
+    print("difference -------------------------------------------------------------")
+
     print("difference sparse, SGT: ", (sparse_Pd - SGT_Pd))
     print("difference sparse, predict_proba: ", (sparse_Pd - predict_proba_Pd))
     print("difference predict_proba, SGT: ", (predict_proba_Pd - SGT_Pd))
 
-trial(model)
+    print("mean square error -------------------------------------------------------------")
+    
+    print("mean square error sparse, SGT: ", mean_squared_error(sparse_Pd, SGT_Pd, squared=False))
+    print("mean square error sparse, predict_proba: ", mean_squared_error(sparse_Pd, predict_proba_Pd, squared=False))
+    print("mean square error predict_proba, SGT: ", mean_squared_error(predict_proba_Pd, SGT_Pd, squared=False))
+
+#trial(gaussianNaiveBayes)
