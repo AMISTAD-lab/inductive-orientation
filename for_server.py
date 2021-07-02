7532d1500069a0493601ab16ca4320408e553352
echen4628 <76180196+echen4628@users.noreply.github.com>
2021-07-02 13:42:02 -0700

    added folder structures and clusters plotting to server code

--- for_server.py
+++ for_server.py
@@ -10,6 +10,7 @@ import seaborn as sns
 # import simple_good_turing
 from sklearn.metrics import mean_squared_error
 from statistics import mean
+import os
 
 import itertools
 import random
@@ -24,9 +25,13 @@ import seaborn as sns
 from sklearn.metrics import mean_squared_error
 from statistics import mean
 
-import setup2
 #import clean_data
 import csv
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from umap import UMAP
+from sklearn.cluster import DBSCAN, KMeans, MeanShift, AgglomerativeClustering
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -62,6 +67,7 @@ MLPclf_1 = MLPClassifier(max_iter=500)
 MLPclf_3 = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=500)
 
 from new_ldm_inductive import *
+import server_setup as setup
 
 
 def get_LDM_PD(list_of_clf, X_train, X_test, y_train, classes=[0,1], num_datasets=5, num_repeat=1, proportion_of_dataset=0.1, sparse=True, data_generation=random_uniform):
@@ -72,30 +78,99 @@ def get_LDM_PD(list_of_clf, X_train, X_test, y_train, classes=[0,1], num_dataset
         LDM_l.append(LDM)
         PD_l.append(computePD(LDM, sparse=sparse))
     return LDM_l, PD_l
+dim_reduc_l = ["PCA", "UMAP-n_neighbors=20", "UMAP-n_neighbors=15", "UMAP-n_neighbors=10", "UMAP-n_neighbors=5"]
+cluster_alg_l = ["DBSCAN-eps=0.35-min_samples=3", "DBSCAN-eps=0.25-min_samples=3", "DBSCAN-eps=0.5-min_samples=3", "DBSCAN-eps=0.10-min_samples=3"
+                    , "AgglomerativeClustering-n_clusters=2", "AgglomerativeClustering-n_clusters=4", "AgglomerativeClustering-n_clusters=8",
+                    "MeanShift"]
+def generate_paths(dim_reduc_l, cluster_alg_l):
+    paths = []
+    for dim_reduc_raw in dim_reduc_l:
+        dim_reduc = dim_reduc_raw.split("-")
+        dim_reduc_function = dim_reduc[0]
+        dim_reduc_parameters = dim_reduc[1:] #might give an error
+        dim_reduc_parameters = {parameter.split("=")[0]:parameter.split("=")[1] for parameter in dim_reduc_parameters}
+        print(dim_reduc_parameters)
+        for cluster_alg_raw in cluster_alg_l:
+            cluster_alg = cluster_alg_raw.split("-")
+            cluster_alg_function = cluster_alg[0]
+            cluster_alg_parameters = cluster_alg[1:]
+            cluster_alg_parameters = {parameter.split("=")[0]:parameter.split("=")[1] for parameter in cluster_alg_parameters}
+            print(cluster_alg_parameters)
+            paths.append(dim_reduc_raw + "|" + cluster_alg_raw)
+    return paths
+
+def convert_str(n):
+    if "." in n:
+        return float(n)
+    else:
+        return int(n)
+
+def generate_plots(pD_vectors,clf_names, list_of_clf, paths, base, markers, dim_reduc_function_dict, cluster_alg_function_dict):
+    for path in paths:
+        dim_reduc = path.split("|")[0]
+        dim_reduc = dim_reduc.split("-")
+        dim_reduc_function = dim_reduc[0]
+        dim_reduc_parameters = dim_reduc[1:]
+        print(dim_reduc_parameters)
+        dim_reduc_parameters = {parameter.split("=")[0]:convert_str(parameter.split("=")[1]) for parameter in dim_reduc_parameters}
+
+        cluster_alg = path.split("|")[1]
+        cluster_alg = cluster_alg.split("-")
+        cluster_alg_function = cluster_alg[0]
+        cluster_alg_parameters = cluster_alg[1:]
+        cluster_alg_parameters = {parameter.split("=")[0]:convert_str(parameter.split("=")[1]) for parameter in cluster_alg_parameters}
+        
+        labels, list_of_PD = setup.cluster(pD_vectors, cluster_alg_function_dict[cluster_alg_function], list_of_clf, cluster_alg_parameters)
+        visualDim = 2
+        reduceDim = True
+        dim_reduc_parameters = {**{"n_components":2} , **dim_reduc_parameters}
+        setup.cluster_plot(list_of_PD, clf_names, os.path.join(base, path).replace("|", "-") , markers, labels, visualDim, reduceDim, dim_reduc_function_dict[dim_reduc_function], dim_reduc_parameters)
+        setup.analyzeResults(os.path.join(base, path).replace("|", "-"), labels, clf_names) #might need to save this to some sort of doc
 
 
 
 def main():
+    current_folders = os.listdir()
+    runs = list(filter(lambda name: name[:3]=="run", current_folders))
+    if len(runs)==0:
+        run_path = "run0"
+    else:
+        runs = [int(run[3:]) for run in runs]
+        run_path = "run" + str(max(runs)+1)
+    os.mkdir(run_path)
+    cluster_path = os.path.join(run_path, "clusters")
+    os.mkdir(cluster_path)
     dataset = pd.read_csv("EEG_Eye_State.csv")
     values = dataset.values
     X, y = values[:, :-1], values[:, -1]
-
-    
+    markers = {'KNN1': "$a$", 'KNN3': "$b$", 'KNN11': "$c$", 'randomForest1': "$d$", 'randomForest5': "$e$", 
+            'randomForest10': "$f$", 'randomForest25': "$g$",'randomForest100': "$h$",'naiveBayesClassifier': "$i$",
+            'adaboostClassifier': "$j$",'gradientBoostingClassifier': "$k$", 'decisionTreeClassifier': "$l$", 
+            'quadraticDiscriminantAnalysis': "$m$", 'logisticRegression': "$n$", 'SGDClassifier_hinge': "$o$", 
+            'SGDClassifier_log': "$p$", 'MLPclf_1': "$q$", 'MLPclf_3': "$r$", "SVC_linear": "$s$", "SVC_rbf": "$t$" }
+    dim_reduc_function_dict = {"PCA": PCA, "UMAP": UMAP}
+    cluster_alg_function_dict = {"DBSCAN":DBSCAN, "AgglomerativeClustering":AgglomerativeClustering, "MeanShift":MeanShift}
     num_repeats = 3
-    list_of_clf=[decisionTreeClassifier]*num_repeats 
+    list_of_clf=[decisionTreeClassifier]*num_repeats + [KNN1]*num_repeats + [KNN11]*num_repeats
+    clf_names = ["decisionTreeClassifier"]*num_repeats + ["KNN1"]*num_repeats + ["KNN11"]*num_repeats
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=5, random_state=42)
     LDM_l, PD_l = get_LDM_PD(list_of_clf, X_train, X_test, y_train, num_datasets=505, num_repeat=1, proportion_of_dataset=0.04)
     
-
-    with open('ldm.txt', 'w') as f:
+    with open(os.path.join(run_path, 'ldm.txt'), 'w') as f:
         csvwriter = csv.writer(f)
         csvwriter.writerows(LDM_l)
 
-    with open('pd.txt', 'w') as f:
+    with open(os.path.join(run_path, 'pd.txt'), 'w') as f:
         csvwriter = csv.writer(f)
         csvwriter.writerows(PD_l)
-
+    
+    dim_reduc_l = ["PCA", "UMAP-n_neighbors=20", "UMAP-n_neighbors=15", "UMAP-n_neighbors=10", "UMAP-n_neighbors=5"]
+    cluster_alg_l = ["DBSCAN-eps=0.35-min_samples=3", "DBSCAN-eps=0.25-min_samples=3", "DBSCAN-eps=0.5-min_samples=3", 
+                    "DBSCAN-eps=0.10-min_samples=3", "AgglomerativeClustering-n_clusters=2", 
+                    "AgglomerativeClustering-n_clusters=4", "AgglomerativeClustering-n_clusters=8","MeanShift"]
+    paths = generate_paths(dim_reduc_l, cluster_alg_l)
+    generate_plots(PD_l,clf_names, list_of_clf, paths, cluster_path, markers, dim_reduc_function_dict, cluster_alg_function_dict)
 
 
 if __name__ == "__main__":
---
+++ server_setup.py
@@ -0,0 +1,230 @@
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+import math
+
+import new_ldm_inductive as ldm_inductive
+
+# dataset = pd.read_csv("EEG_Eye_State.csv", header = None)
+# values = dataset.values
+# X, y = values[:, :-1], values[:, -1]
+
+classes=[0,1]
+num_datasets=205
+proportion_of_dataset=0.04
+
+#get a list of PDs for different algorithms
+def get_PD(list_of_clf, num_datasets, proportion_of_dataset, X, y, sgt=False, use_predict_proba=False, num_repeat=1):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=5, random_state=42)
+    list_PD = []
+    for clf in list_of_clf:
+        if use_predict_proba:
+            sgt=False
+            LDM = ldm_inductive.getLDM(clf, X_train, X_test, y_train, classes=classes, num_datasets = num_datasets, proportion_of_dataset=proportion_of_dataset, sparse=False, num_repeat=num_repeat)
+        else:
+            LDM = ldm_inductive.getLDM(clf, X_train, X_test, y_train, classes=classes, num_datasets = num_datasets, proportion_of_dataset=proportion_of_dataset, num_repeat=num_repeat)
+               
+        if sgt:
+            Pd = ldm_inductive.simpleGoodTuring(LDM)
+        else:
+            Pd = ldm_inductive.computePD(LDM)
+
+        list_PD.append(Pd)
+    
+    return list_PD
+
+# DBSCAN gives noisy samples the label -1.
+'''
+cluster
+Assumes that dimensionality of the PD vectors will be reduced
+Note: Dimensionality of PD vectors will NOT always be reduced to 2 dimensions...
+Parameters:
+    1. param_dimReduc = a dictionary containing key-value pairs for dimensionality reduction 
+    2. param_clustering = a dictionary containing key-value pairs for clustering algorithm
+'''
+def cluster(list_of_PD, clustering_method, list_of_clf, param_clustering = {}, dimReduc_method=None, param_dimReduc = {},
+            sgt=False, use_predict_proba=False):
+ 
+    #list_of_PD = get_PD(list_of_clf, sgt=sgt, use_predict_proba=use_predict_proba)
+    
+    if dimReduc_method != None:
+        print("param_dimReduc", param_dimReduc)
+        print("param_clustering", param_clustering)
+        #reduce the dimensions of PD vector
+        list_of_PD = dimReduc_method(**param_dimReduc).fit_transform(np.array(list_of_PD))
+    
+    #cluster lower dimensional PD vectors 
+    clustering = clustering_method(**param_clustering).fit(list_of_PD)
+    labels = clustering.labels_
+    
+    return labels, list_of_PD
+ 
+'''
+cluster_plot
+Parameters:
+    1. X = embedded_PD's
+    2. clf_names = names of the classifiers
+    3. filepath = where to save the pdf
+    3. labels = different clusters formed
+    4. visualDim = # of dimensions that clusters + Pd vectors will be visualized as (either 2 or 3)
+Note: X may not be in 2D, 3D
+    5. reduceDim = False if X = embedded_PD's do NOT need to be reduced 
+                 = True if X = embedded_PD's need to be reduced 
+    6. *args = dim_Reduction_method 
+    7. *kwargs = key-value parameters for dim_Reduction 
+'''
+def cluster_plot(X, clf_names, filepath, markers, labels = [], visualDim = 2, reduceDim = False, dimReduc_method=None, param_dimReduc = {}):   
+    #fixes the color for the labels
+    palette = {0:"#F23030", 1:"#F2762E", 2:"#F2D338", 3:"#25C7D9", 4:"#248EA6", -1:"#324DBE", 5:"#800080", 6:"#3C005A", 7:"#EABFFF"} 
+    #X needs to be reduced to a lower dimension 2D
+    if reduceDim:
+        print("args: ",  dimReduc_method)
+        print("type args: ",  type(dimReduc_method))
+        print("param_dimReduc: ", param_dimReduc)
+        X = dimReduc_method(**param_dimReduc).fit_transform(np.array(X))
+    
+    #check that the dimensions of the reduced PD vectors is equal to the dimension
+    print("X", X)
+    print("visualDim", visualDim)
+    print("len(X[0])", len(X[0]))
+    if len(X[0]) != visualDim:
+        raise Exception("Dimension of Reduced PD vectors must equal to the dimension being visualized!")
+        
+    if visualDim == 2:
+        
+        if len(labels) != 0:
+            fig, ax = plt.subplots(figsize=(5,5))
+            ax.set_position([0.1,0.1,0.8,0.8])
+            df = pd.DataFrame({'x1': X[:, 0], 'x2': X[:, 1], 'label': labels, 'clf_name': clf_names})
+            #print(df)
+            sns.scatterplot(data=df, x="x1", y="x2", hue="label", style="clf_name",  legend = "full", s = 100, ax=ax, markers=markers, palette=palette)
+            plt.legend(bbox_to_anchor=(1.05, 0.1), borderaxespad=0, loc="lower left")
+            plt.savefig(filepath + ".pdf", bbox_inches="tight", pad_inches=0)
+            for label in np.unique(np.array(labels)):
+              print(label)
+              df_with_label = df[df["label"] == label]
+              fig, ax = plt.subplots(figsize=(5,5))
+              ax.set_position([0.1,0.1,0.8,0.8])
+              sns.scatterplot(data=df_with_label, x="x1", y="x2", hue="label", legend="full", style="clf_name", s = 100, ax=ax, markers=markers, palette=palette)
+              # handles, labels = plt.gca().get_legend_handles_labels()
+              # by_label = dict(zip(labels, handles))
+              # plt.legend(by_label.values(), by_label.keys())
+              plt.legend(bbox_to_anchor=(1.05, 0.1), borderaxespad=0, loc="lower left")
+              plt.savefig(filepath + "-" + str(label) +".pdf", bbox_inches="tight", pad_inches=0)
+        else:
+            fig, ax = plt.subplots(figsize=(10,10))
+            ax.set_position([0.1,0.1,0.5,0.8])
+            df = pd.DataFrame({'x1': X[:, 0], 'x2': X[:, 1], 'clf_name': clf_names})
+            #print(df)
+            sns.scatterplot(data=df, x="x1", y="x2", style="clf_name",  legend = "full", s =100, ax=ax)  
+            plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0)
+            plt.savefig(filepath + ".pdf", bbox_inches="tight", pad_inches=0)
+            for label in np.unique(np.array(labels)):
+              df_with_label = df[df["label"] == label]
+              sns.scatterplot(data=df_with_label, x="x1", y="x2", style="clf_name",  legend = "full", s = 100)
+              plt.legend(bbox_to_anchor=(1, 0.5), borderaxespad=0)
+              plt.savefig(filepath + str(label) +".pdf", bbox_inches="tight", pad_inches=0)
+
+    elif visualDim == 3:
+        
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'tab:blue', 'tab:orange', 'tab:gray', 'tab:red',
+         'tab:purple', 'tab:brown', 'tab:pink', 'tab:cyan']
+
+        #allTypesOfMarkers = dictionary
+        allTypesOfMarkers = list(mpl.markers.MarkerStyle.markers.keys())
+        #unique classifers: unique_clfs
+        unique_clfs = np.unique(clf_names)
+        markerStyles = allTypesOfMarkers[:len(unique_clfs)]
+        
+        #map classifier to unique marker
+        mapClfsToMarker = {}
+        for c, m in zip(unique_clfs, markerStyles):
+            mapClfsToMarker[c] = m
+            
+        print("Map Classifier to Marker: ", mapClfsToMarker)
+        
+        clfMarkerStyle = []
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        
+        x = X[:, 0]
+        y = X[:, 1]
+        z = X[:, 2]
+        
+        if len(labels) != 0:
+            color_label = colors[:len(np.unique(labels))] 
+             #print("labels:",  labels)
+        
+            mapLabelsToColor = {}
+            for l, c in zip(np.unique(labels), color_label):
+                mapLabelsToColor[l] = c
+        
+            print("Map Labels to Color: ", mapLabelsToColor)
+        
+            clfColor = []
+            for c, l in zip(clf_names, labels):
+                clfMarkerStyle.append(mapClfsToMarker[c])
+                clfColor.append(mapLabelsToColor[l])
+        
+            #print("clfColor", clfColor)
+
+            for i in range(len(clf_names)):
+                ax.scatter(x[i], y[i], z[i], c = clfColor[i], marker= clfMarkerStyle[i], s = 100)
+        else:
+            for c in clf_names:
+                clfMarkerStyle.append(mapClfsToMarker[c])
+            
+            for i in range(len(clf_names)):
+                ax.scatter(x[i], y[i], z[i], marker= clfMarkerStyle[i], color = "red", s = 100)
+                
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        plt.show()
+    else:
+        print(visualDim, "canNOT be visualized.")
+
+def analyzeResults(path, label, clfNames):
+  tableClusters = pd.DataFrame({'Labels': label, 'Algorithms': clfNames})
+  tableClusters = tableClusters.groupby(['Labels','Algorithms'])['Algorithms'].count()
+  tableClusters.to_csv(path, index=True)
+    
+def plotPairwiseDist(list_of_PDs, clf_Names):
+  list_of_PDs = np.array(list_of_PDs)
+  pairwise_Dist = np.sum((list_of_PDs[:, np.newaxis, :] - list_of_PDs[np.newaxis, :, :])**2, axis = -1)
+  df_pairwise_Dist = pd.DataFrame(pairwise_Dist, index = clf_Names, columns = clf_Names)
+  fig, ax = plt.subplots(figsize=(12,12)) 
+  sns.heatmap(df_pairwise_Dist, annot = True, cmap="YlGnBu", ax=ax)
+  
+def plot_pairwise_angles(list_of_pDs, clf_Names): 
+  index = np.arange(len(list_of_pDs))
+  pairwise_combos = np.array(list(itertools.combinations(index, 2)))
+
+  angles = []
+  for i in range(len(pairwise_combos)):
+    tuple_index = pairwise_combos[i]
+    dot_product = 0
+    len_a = 0
+    len_b = 0
+    for a, b in zip(list_of_pDs[tuple_index[0]], list_of_pDs[tuple_index[1]]):
+      dot_product += a * b
+      len_a += a * a
+      len_b += b * b 
+    angles.append(math.acos(dot_product / (math.sqrt(len_a) * math.sqrt(len_b))))
+
+  pairwise_angles = np.zeros((len(list_of_pDs), len(list_of_pDs)))
+  for j in range(len(angles)):
+    a = pairwise_combos[j][0]
+    b = pairwise_combos[j][1]
+    pairwise_angles[a][b] = angles[j]
+    pairwise_angles[b][a] = angles[j]
+
+  df_pairwise_Angles = pd.DataFrame(pairwise_angles, index = clf_Names, columns = clf_Names)
+  sns.heatmap(df_pairwise_Angles, annot = True, cmap="YlGnBu")
