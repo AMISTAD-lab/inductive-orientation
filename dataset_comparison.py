fb7601b1c7d9c36970f43e8af1d06789e6512d18
Sophie Bekerman <sophiebekerman@sophies-mbp.lan>
2021-07-07 11:21:40 -0700

    added max_iter to svc in for_server, wrote dataset comparison for letter dataset

---
+++ dataset_comparison.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+dataset = pd.read_csv("letters_cleaned.csv", header = None)
+values = dataset.values
+X, y = values[:, :-1], values[:, -1]
+
+
+def convert_to_np(dataset):
+    values = dataset.values
+    X, y = values[:, :-1], values[:, -1]
+    points = np.array(X)
+    return points
+
+
+def get_avg_dist(dataset):
+    points = convert_to_np(dataset)
+    #print("points ", points)
+    pairwise_Dist = np.sqrt(np.sum((points[:, np.newaxis, :] - points[np.newaxis, :, :])**2, axis = -1))
+    #print("dist ", pairwise_Dist)
+    return get_min_dist(pairwise_Dist)
+
+
+def get_min_dist(pairwise_Dist):
+    min_dist = np.where(pairwise_Dist>0, pairwise_Dist, np.inf).min(axis=1)
+    avg_dist = np.average(min_dist)
+    if avg_dist == np.inf:
+        avg_dist = 0
+
+    return avg_dist
+
+print("distance to any class label ", get_avg_dist(dataset))
+
+#get dist same class label
+dataset0 = dataset.loc[y == 0]
+dataset1 = dataset.loc[y == 1]
+
+
+avg_dist_same = (get_avg_dist(dataset0) + get_avg_dist(dataset1))/2
+print("distance to same class label ", avg_dist_same)
+
+
+points0 = convert_to_np(dataset0)
+points1 = convert_to_np(dataset1)
+
+#get dist same class label
+dist_diff = cdist(points0, points1)
+avg_diff_dist = get_min_dist(dist_diff)
+print("distance to different class label ", avg_diff_dist)
+
