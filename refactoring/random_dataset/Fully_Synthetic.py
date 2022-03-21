7924adc22fa5ba2c7b3a55cfb9f944a19f0af285
echen4628 <echen4628@gmail.com>
2022-03-21 06:03:35 -0700

    trying fully synthetic data

---
+++ refactoring/random_dataset/Fully_Synthetic.py
@@ -0,0 +1,10 @@
+# 2 to 12 dimensions; size of dataset vary between 100 to 5000 samples
+
+import numpy as np
+
+def generate_fully_synethic(n_features, n_samples, max_value, seed=42):
+    rng = np.random.default_rng(seed)
+    x_data = rng.integers(low=0, high=max_value, size=(n_samples, n_features))
+    y_data = rng.integers(low=0, high=2, size=n_samples)
+    return x_data, y_data
+
