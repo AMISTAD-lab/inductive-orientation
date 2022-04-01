f6ababc5eb6a2c75d0037ff5af1ec7bf0ff31f50
echen4628 <echen4628@gmail.com>
2022-04-01 05:02:29 -0700

    allowed for changes in number of classes.

--- refactoring/random_dataset/Fully_Synthetic.py
+++ refactoring/random_dataset/Fully_Synthetic.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-def generate_fully_synethic(n_features, n_samples, max_value, seed=42):
+def generate_fully_synethic(n_features, n_samples, max_value, num_classes, seed=42):
     rng = np.random.default_rng(seed)
     x_data = rng.integers(low=0, high=max_value, size=(n_samples, n_features))
-    y_data = rng.integers(low=0, high=2, size=n_samples)
+    y_data = rng.integers(low=0, high=num_classes, size=n_samples)
     return x_data, y_data
 
